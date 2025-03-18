190cdb28532fa6a8a87920b4b8017a884ef53411
dagota12 <chernetdagim@gmail.com>
2025-03-19 01:22:10 +0300

    feat: add About and AnimatedTitle components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import { Hero } from "./components/Hero";
 
 function App() {
   return (
     <main className="relative min-h-screen overflow-x-hidden">
       <Hero />
+      <About />
       <section className="w-full h-screen bg-violet-200"></section>
     </main>
   );
