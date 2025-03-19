859727506793cc79d974fc7d3e328de6069392a2
dagota12 <chernetdagim@gmail.com>
2025-03-19 15:25:26 +0300

    feat: add Story component implement mouse interaction effects

--- src/components/AnimatedTitle.tsx
+++ src/components/AnimatedTitle.tsx
@@ -28,7 +28,7 @@ const AnimatedTitle = ({ title, containerClass }: Props) => {
           opacity: 1,
           transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
           ease: "power2.inOut",
-          stagger: 0.2,
+          stagger: 0.02,
         },
         0
       );
---
+++ src/components/Story.tsx
@@ -0,0 +1,85 @@
+import { useRef } from "react";
+import AnimatedTitle from "./AnimatedTitle";
+import gsap from "gsap";
+import RoundedCorners from "./RoundedCorners";
+import Button from "./Button";
+
+const Story = () => {
+  const frameRef = useRef<HTMLImageElement>(null);
+
+  const handleMouseLeave = () => {};
+  const handleMouseMove = (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>
+  ) => {
+    const { clientX, clientY } = e;
+
+    const element = frameRef.current;
+    if (!element) return;
+
+    const rect = element.getBoundingClientRect();
+
+    const x = clientX - rect.left;
+    const y = clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10;
+    const rotateY = ((x - centerX) / centerY) * 10;
+
+    gsap.to(element, {
+      duration: 0.3,
+      rotateX,
+      rotateY,
+      transformPerspective: 500,
+      ease: "power1.inOut",
+    });
+  };
+  return (
+    <section id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10">
+        <div className="relative size-full">
+          <AnimatedTitle
+            // sectionId="#story"
+            title="The Story of a hidden relm "
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  onMouseUp={handleMouseLeave}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                  onMouseMove={handleMouseMove}
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="Entrance"
+                  className="object-contain"
+                />
+              </div>
+            </div>
+            <RoundedCorners />
+          </div>
+        </div>
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-14 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt-5 text-black"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Story;
